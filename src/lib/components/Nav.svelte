4002faccade757fa700fb56e5a1799d0e0404e04
Nayomekaia <nayomedoelwijt@gmail.com>
2026-06-07 13:47:29 +0200

    added nav
    #9

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,1 +1,21 @@
+<script>
 	import logo from '$lib/assets/logo.svg';
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="/" class="logo">
+				<img src={logo} alt="CRE8 Studios logo" />
+			</a>
+		</li>
+
+		<li>
+			<a href="/archive" class:active={isActive('/archive')}>Archive</a>
+		</li>
+
+		<li>
+			<a href="/information" class:active={isActive('/information')}>Information</a>
+		</li>
+	</ul>
+</nav>
