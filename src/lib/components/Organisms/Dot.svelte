69c6474a41c356e0f3adc774b446233fdff92857
Nayomekaia <nayomedoelwijt@gmail.com>
2026-06-09 00:18:43 +0200

    add canvas state variables
    #26

--- src/lib/components/Organisms/Dot.svelte
+++ src/lib/components/Organisms/Dot.svelte
@@ -1,6 +1,14 @@
 <script>
 	import { onMount } from 'svelte';
 	import logoSvg from '$lib/assets/logo.svg';
+
+	let canvas;
+	let ctx;
+	let animationFrame;
+	let particles = [];
+	let logoImage;
+	let prefersReducedMotion = false;
+
 </script>
 <section class="dotted-word-section">
 </section>
